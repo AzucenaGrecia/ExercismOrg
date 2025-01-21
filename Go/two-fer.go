41af3a4ada1d89357bfad766e0f3c88431623866
Grecia Delgado <greciadm2596@gmail.com>
2025-01-20 23:02:20 -0500

    Add new solition

--- Go/two-fer.go
+++ Go/two-fer.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-func share(name string) string {
-	if name == "" {
-		name = "you"
+
+func ShareWith(name string) string {
+	msg := `One for %s, one for me.`
+	if len(name) > 0 {
+		return fmt.Sprintf(msg, name)
 	}
-	return fmt.Sprintf("one for " + name + ", one for me.")
+	return fmt.Sprintf(msg, "you")
 }
